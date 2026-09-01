67f7403b56be7e8e90354627415597c71811a7ed
Arisgav <amgavarrete@uamv.edu.ni>
2026-09-01 15:26:02 -0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ni.edu.uam.pae_eventos_javafx_equipo_2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ni.edu.uam.pae_eventos_javafx_equipo_2 to javafx.fxml;
+    exports ni.edu.uam.pae_eventos_javafx_equipo_2;
+}
