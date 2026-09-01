23ceb952181d257161ee2b8855c6a4e47d8ec781
Arisgav <amgavarrete@uamv.edu.ni>
2026-09-01 16:22:58 -0600

    Reto3 y Main

--- src/main/java/ni/edu/uam/pae_eventos_javafx_equipo_2/Main.java
+++ src/main/java/ni/edu/uam/pae_eventos_javafx_equipo_2/Main.java
@@ -1,4 +1,24 @@
 package ni.edu.uam.pae_eventos_javafx_equipo_2;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Carga la estructura base (el BorderPane con el menú lateral)
+        Parent root = FXMLLoader.load(getClass().getResource("/views/Main.fxml"));
+
+        primaryStage.setTitle("Ejercicios en Pareja - JavaFX");
+        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/ni/edu/uam/pae_eventos_javafx_equipo_2/controllers/Reto3Controller.java
+++ src/main/java/ni/edu/uam/pae_eventos_javafx_equipo_2/controllers/Reto3Controller.java
@@ -1,4 +1,34 @@
 package ni.edu.uam.pae_eventos_javafx_equipo_2.controllers;
 
+
+import ni.edu.uam.pae_eventos_javafx_equipo_2.models.Artesania;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class Reto3Controller {
+    @FXML private TableView<Artesania> tablaArtesanias;
+    @FXML private TableColumn<Artesania, String> colImagen;
+    @FXML private TableColumn<Artesania, String> colNombre;
+
+    @FXML
+    public void initialize() {
+        colImagen.setCellValueFactory(new PropertyValueFactory<>("imagen"));
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+
+        tablaArtesanias.getItems().addAll(
+                new Artesania("[ \uD83C\uDFA8 ]", "Hamaca Tradicional"),
+                new Artesania("[ \uD83C\uDFFA ]", "Jarrón de Cerámica")
+        );
+    }
+
+    @FXML void accionNuevo() { mostrarMensaje("Acción: Nuevo producto"); }
+    @FXML void accionGuardar() { mostrarMensaje("Acción: Guardando..."); }
+    @FXML void accionBuscar() { mostrarMensaje("Acción: Buscando..."); }
+
+    private void mostrarMensaje(String msg) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, msg);
+        alert.setHeaderText(null);
+        alert.showAndWait();
+    }
 }
