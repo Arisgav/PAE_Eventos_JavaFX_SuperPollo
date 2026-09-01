24478bc6a5e19f5b460b8102e2ebcd8347c4315a
Arisgav <amgavarrete@uamv.edu.ni>
2026-09-01 16:04:50 -0600

    Main controller

--- src/main/java/ni/edu/uam/pae_eventos_javafx_equipo_2/controllers/MainController.java
+++ src/main/java/ni/edu/uam/pae_eventos_javafx_equipo_2/controllers/MainController.java
@@ -1,4 +1,25 @@
 package ni.edu.uam.pae_eventos_javafx_equipo_2.controllers;
 
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import java.io.IOException;
+
 public class MainController {
+    @FXML private BorderPane mainLayout;
+
+    @FXML void cargarReto1() { cargarVista("/views/Reto1.fxml"); }
+    @FXML void cargarReto2() { cargarVista("/views/Reto2.fxml"); }
+    @FXML void cargarReto3() { cargarVista("/views/Reto3.fxml"); }
+
+    private void cargarVista(String fxml) {
+        try {
+            VBox vista = FXMLLoader.load(getClass().getResource(fxml));
+            mainLayout.setCenter(vista);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
