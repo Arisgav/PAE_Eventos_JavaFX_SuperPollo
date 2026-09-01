a08ef97cfb56b3b57cce794c0c9ee9606523f129
moraditoo <damora@uamv.edu.ni>
2026-09-01 15:51:36 -0600

    chore: inicializar proyecto y estructura MVC

--- src/main/java/ni/edu/uam/pae_eventos_javafx_equipo_2/Launcher.java
+++
@@ -1,9 +0,0 @@
-package ni.edu.uam.pae_eventos_javafx_equipo_2;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
---
+++ src/main/java/ni/edu/uam/pae_eventos_javafx_equipo_2/controllers/Reto1Controller.java
@@ -0,0 +1,4 @@
+package ni.edu.uam.pae_eventos_javafx_equipo_2.controllers;
+
+public class Reto1Controller {
+}
